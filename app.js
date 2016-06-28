53f08a51e77560ec3d1bca6810ba7ad79558eb17
philippschulte <philipp.schulte@ymail.com>
2016-06-28 13:54:10 -0600

    Add error handling

--- app.js
+++ app.js
@@ -9,22 +9,35 @@ var accountCtrl = require('./controllers/accountCtrl.js');
 // Command
 var command = account._[0];
 
-if (command === 'create') {
-  var newAccount = accountCtrl.create({
-    name: account.name,
-    username: account.username,
-    password: account.password
-  }, account.masterPassword);
+switch (command) {
+  case 'create':
+    try {
+      var newAccount = accountCtrl.create({
+        name: account.name,
+        username: account.username,
+        password: account.password
+      }, account.masterPassword);
 
-  console.log('New account created!');
-  console.log(newAccount);
-} else if (command === 'get') {
-  var fetchedAccount = accountCtrl.read(account.name, account.masterPassword);
+      console.log('New account created!');
+      console.log(newAccount);
+    } catch(e) {
+      console.log('Unable to create account!');
+    }
 
-  if (typeof fetchedAccount === 'undefined') {
-    console.log('Account not found!');
-  } else {
-    console.log('Account found!');
-    console.log(fetchedAccount);
-  }
+    break;
+  case 'get':
+    try {
+      var fetchedAccount = accountCtrl.read(account.name, account.masterPassword);
+
+      if (typeof fetchedAccount === 'undefined') {
+        console.log('Account not found!');
+      } else {
+        console.log('Account found!');
+        console.log(fetchedAccount);
+      }
+    } catch(e) {
+      console.log('Unable to fetch account!');
+    }
+
+    break;
 }
