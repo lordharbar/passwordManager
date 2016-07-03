e450327ac8b2c41685820396aa154d0c4c033229
philippschulte <philipp.schulte@ymail.com>
2016-07-03 15:41:39 -0600

    Add tests for read, update, and delete command

--- test/validate.test.js
+++ test/validate.test.js
@@ -29,4 +29,28 @@ describe('validate', function() {
     }, account.masterPassword);
   });
 
+  it('should call accountCtrl.read on read command', function() {
+    var spy = expect.spyOn(accountCtrl, 'read');
+
+    validate('read', account, true);
+
+    expect(spy).toHaveBeenCalledWith(account.name, account.masterPassword);
+  });
+
+  it('should call accountCtrl.update on update command', function() {
+    var spy = expect.spyOn(accountCtrl, 'update');
+
+    validate('update', account, true);
+
+    expect(spy).toHaveBeenCalledWith(account.name, account.username, account.password, account.masterPassword);
+  });
+
+  it('should call accountCtrl.delete on delete command', function() {
+    var spy = expect.spyOn(accountCtrl, 'delete');
+
+    validate('delete', account, true);
+
+    expect(spy).toHaveBeenCalledWith(account.name, account.masterPassword);
+  });
+
 });
