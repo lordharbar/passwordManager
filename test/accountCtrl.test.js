38bb0442a4bd65defc6c1a3a670e95bb0d388bae
philippschulte <philipp.schulte@ymail.com>
2016-07-12 11:41:05 -0600

    Refactor accountCtrl.test with sinon assertions

--- test/accountCtrl.test.js
+++ test/accountCtrl.test.js
@@ -1,9 +1,15 @@
 'use strict';
 
 // Dependencies
-var expect = require('expect');
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
 var accountCtrl = require('../controllers/accountCtrl.js');
 var storageCtrl = require('../controllers/storageCtrl.js');
+
+// Chai setup
+chai.should();
+chai.use(sinonChai);
 
 describe('accountCtrl', function() {
   var account = {
@@ -13,143 +19,152 @@ describe('accountCtrl', function() {
     masterPassword: 'master123'
   };
 
-  var spy_1 = expect.spyOn(storageCtrl, 'get');
-  var spy_2 = expect.spyOn(storageCtrl, 'set');
-
-  beforeEach(function() {
-    spy_1.andCallThrough();
-    spy_2.andCallThrough();
-  });
-
-  it('should exist', function() {
-    expect(accountCtrl).toExist();
-  });
-
-  describe('create', function() {
-    afterEach(function () {
-      spy_1.restore();
-      spy_2.restore();
-
+  describe('#create', function() {
+    afterEach(function() {
       accountCtrl.delete(account.name, account.masterPassword);
     });
 
-    it('should save the new account locally', function() {
-      accountCtrl.create({
-        name: account.name,
-        username: account.username,
-        password: account.password
-      }, account.masterPassword);
+    it('should call storageCtrl.get once', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
+      get.should.have.been.calledOnce;
+    }));
 
-      var matchedAccount = accountCtrl.read(account.name, account.masterPassword);
+    it('should call storageCtrl.get with the master password', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
+      get.should.have.been.calledWithExactly(account.masterPassword);
+    }));
 
-      expect(matchedAccount).toEqual({name: account.name, username: account.username, password: account.password});
-    });
+    it('should call storageCtrl.set once', sinon.test(function() {
+      var set = this.spy(storageCtrl, 'set');
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
+      set.should.have.been.calledOnce;
+    }));
 
-    it('should return the new account', function() {
-      var newAccount = accountCtrl.create({
-        name: account.name,
-        username: account.username,
-        password: account.password
-      }, account.masterPassword);
+    it('should call storageCtrl.set with all accounts and with the master password', sinon.test(function() {
+      var set = this.spy(storageCtrl, 'set');
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
+      set.should.have.been.calledWithExactly([{name: account.name, username: account.username, password: account.password}], account.masterPassword);
+    }));
 
-      expect(newAccount).toEqual({name: account.name, username: account.username, password: account.password});
-    });
+    it('should return the new account', sinon.test(function() {
+      var create = this.spy(accountCtrl, 'create');
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
+      create.should.have.returned({name: account.name, username: account.username, password: account.password});
+    }));
   });
 
-  describe('read', function() {
+  describe('#read', function() {
     before(function() {
-      accountCtrl.create({
-        name: account.name,
-        username: account.username,
-        password: account.password
-      }, account.masterPassword);
-    });
-
-    afterEach(function () {
-      spy_1.restore();
-      spy_2.restore();
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
     });
 
     after(function() {
       accountCtrl.delete(account.name, account.masterPassword);
     });
 
-    it('should return the account if it exists', function() {
-      var matchedAccount = accountCtrl.read(account.name, account.masterPassword);
+    it('should call storageCtrl.get once', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.read(account.name, account.masterPassword);
+      get.should.have.been.calledOnce;
+    }));
 
-      expect(matchedAccount).toEqual({name: account.name, username: account.username, password: account.password});
-    });
+    it('should call storageCtrl.get with the master password', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.read(account.name, account.masterPassword);
+      get.should.have.been.calledWithExactly(account.masterPassword);
+    }));
 
-    it('should return null if the account does not exist', function() {
-      var matchedAccount = accountCtrl.read('facebook', account.masterPassword);
+    it('should return the matched account if it exists', sinon.test(function() {
+      var read = this.spy(accountCtrl, 'read');
+      accountCtrl.read(account.name, account.masterPassword);
+      read.should.have.returned({name: account.name, username: account.username, password: account.password});
+    }));
 
-      expect(matchedAccount).toEqual(null);
-    });
+    it('should return null if the account does not exist', sinon.test(function() {
+      var read = this.spy(accountCtrl, 'read');
+      accountCtrl.read('facebook', account.masterPassword);
+      read.should.have.returned(null);
+    }));
   });
 
-  describe('update', function() {
+  describe('#update', function() {
     before(function() {
-      accountCtrl.create({
-        name: account.name,
-        username: account.username,
-        password: account.password
-      }, account.masterPassword);
-    });
-
-    afterEach(function () {
-      spy_1.restore();
-      spy_2.restore();
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
     });
 
     after(function() {
       accountCtrl.delete(account.name, account.masterPassword);
     });
 
-    it('should update the account if it exists', function() {
-      var updatedAccount = accountCtrl.update(account.name, '@philippschulte', 'tweets123', account.masterPassword);
+    it('should call storageCtrl.get once', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.update(account.name, '@philippschulte', 'tweets123', account.masterPassword);
+      get.should.have.been.calledOnce;
+    }));
 
-      expect(updatedAccount).toNotEqual(account);
-      expect(updatedAccount).toEqual({name: account.name, username: '@philippschulte', password: 'tweets123'});
-    });
+    it('should call storageCtrl.get with the master password', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.update(account.name, '@philippschulte', 'tweets123', account.masterPassword);
+      get.should.have.been.calledWithExactly(account.masterPassword);
+    }));
 
-    it('should return null if the account does not exist', function() {
-      var updatedAccount = accountCtrl.update('facebook', 'Philipp Schulte', 'facebook123', account.masterPassword);
+    it('should return the updated account if it exists', sinon.test(function() {
+      var update = this.spy(accountCtrl, 'update');
+      accountCtrl.update(account.name, '@philippschulte', 'tweets123', account.masterPassword);
+      update.should.have.returned({name: account.name, username: '@philippschulte', password: 'tweets123'});
+    }));
 
-      expect(updatedAccount).toEqual(null);
-    });
+    it('should return null if the account does not exist', sinon.test(function() {
+      var update = this.spy(accountCtrl, 'update');
+      accountCtrl.update('facebook', 'Philipp Schulte', 'facebook123', account.masterPassword);
+      update.should.have.returned(null);
+    }));
   });
 
-  describe('delete', function() {
-    before(function() {
-      accountCtrl.create({
-        name: account.name,
-        username: account.username,
-        password: account.password
-      }, account.masterPassword);
+  describe('#delete', function() {
+    beforeEach(function() {
+      accountCtrl.create({name: account.name, username: account.username, password: account.password}, account.masterPassword);
     });
 
-    afterEach(function () {
-      spy_1.restore();
-      spy_2.restore();
-    });
-
-    after(function() {
+    afterEach(function() {
       accountCtrl.delete(account.name, account.masterPassword);
     });
 
-    it('should delete the account if it exists', function() {
+    it('should call storageCtrl.get once', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
       accountCtrl.delete(account.name, account.masterPassword);
+      get.should.have.been.calledOnce;
+    }));
 
-      var matchedAccount = accountCtrl.read(account.name, account.masterPassword);
+    it('should call storageCtrl.get with the master password', sinon.test(function() {
+      var get = this.spy(storageCtrl, 'get');
+      accountCtrl.delete(account.name, account.masterPassword);
+      get.should.have.been.calledWithExactly(account.masterPassword);
+    }));
 
-      expect(matchedAccount).toEqual(null);
-    });
+    it('should delete the account if it exists', sinon.test(function() {
+      var read = this.spy(accountCtrl, 'read');
+      accountCtrl.read(account.name, account.masterPassword);
+      read.should.have.returned({name: account.name, username: account.username, password: account.password});
+      read.reset();
+      accountCtrl.delete(account.name, account.masterPassword);
+      accountCtrl.read(account.name, account.masterPassword);
+      read.should.have.returned(null);
+    }));
 
-    it('should return null if the account does not exist', function() {
-      var deletedAccount = accountCtrl.delete('facebook', account.masterPassword);
+    it('should return the deleted account if it exists', sinon.test(function() {
+      var destroy = this.spy(accountCtrl, 'delete');
+      accountCtrl.delete(account.name, account.masterPassword);
+      destroy.should.have.returned({name: account.name, username: account.username, password: account.password});
+    }));
 
-      expect(deletedAccount).toEqual(null);
-    });
+    it('should return null if the account does not exist', sinon.test(function() {
+      var destroy = this.spy(accountCtrl, 'delete');
+      accountCtrl.delete('facebook', account.masterPassword);
+      destroy.should.have.returned(null);
+    }));
   });
 
 });
